4ae97373f8922772edf8c07928721b7e94969a47
Alexandra Dzygal <sdzygal@gmail.com>
2023-01-04 17:46:23 -0700

    Create custom lists using Express Route Params

--- app.js
+++ app.js
@@ -35,6 +35,13 @@ const item3 = new Item({
 const defaultItems = [item1, item2, item3];
 
 
+const listSchema = {
+    name: String,
+    items: [itemsSchema]
+};
+
+const List = mongoose.model("List", listSchema);
+
 
 app.get("/", function(req, res) {
 
@@ -52,6 +59,27 @@ app.get("/", function(req, res) {
             res.render("list", {listTitle: "Today", newListItems: foundItems});
         }
     });
+
+});
+
+app.get("/:customListName", function(req, res) {
+ const customListName = req.params.customListName;
+
+ List.findOne({name: customListName}, function(err, foundList) {
+    if (!err) {
+        if (!foundList) { 
+            const list = new List({
+                name: customListName,
+                items: defaultItems
+             });
+            
+             list.save();
+             res.redirect("/" + customListName);
+        } else {
+            res.render("list", {listTitle: foundList.name, newListItems: foundList.items})
+        }
+    }
+ });
 
 });
 
@@ -76,10 +104,7 @@ app.post("/", function(req, res) {
             }
         });
     });
-    
-    app.get("/work", function(req,res){
-        res.render("list", {listTitle: "Work List", newListItems: workItems});
-      });
+
       
       app.get("/about", function(req, res){
         res.render("about");
